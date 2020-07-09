9848be3140fceb07c1d62659f20cf0fa21708227
Saeed Shareef <saeedhshareef@gmail.com>
2020-07-08 21:56:53 -0400

    objectsFunctions

---
+++ js/app.js
@@ -0,0 +1,47 @@
+function newSum (argumentOne,argumentTwo) {
+        return argumentOne + argumentTwo;
+}
+              console.log(newSum(5,10));
+
+
+function addThree (argumentOne,argumentTwo,argumentThree) {
+            return argumentOne + argumentTwo+ argumentThree;
+}
+            console.log(addThree(5,10,15));
+
+function addFour(argumentOne,argumentTwo,argumentThree,argumentFour) {
+        return argumentOne + argumentTwo + argumentThree + argumentFour;
+
+}
+        console.log(addFour(5,10,15,20));
+
+        var myTurtle = {
+
+                "name": "slowpoke",
+                "legs": 4,
+                "tails": 1,
+                "shell": "brown"
+                    };
+
+        var myBird = {
+
+            "name": "tweety",
+            "feathers": "yellow",
+            "tongue": "pink",
+            "eyes": "blue"
+                    };
+                    
+        var myHomie = {
+                "name": "batman",
+                "main": "Falcon",
+                "skills": "crazy"
+                    };
+
+                    console.log(myHomie.name);
+
+                    console.log(myBird["feathers"]);
+
+                    myHomie.main = "Kirby"; 
+
+                    myHomie.secondary = "Link";
+                    
